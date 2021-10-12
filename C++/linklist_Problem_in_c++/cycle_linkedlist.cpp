66d2ab622a7b3997719e83442e4f533f0b44989a
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2021-10-12 14:11:25 +0530

    Create cycle_linkedlist.cpp

---
+++ C++/linklist_Problem_in_c++/cycle_linkedlist.cpp
@@ -0,0 +1,99 @@
+
+// cycle linkedList... 
+// make cycle && detect cycle and remove cycle function...
+
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+    node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+void insertathead(node* &head,int val){
+    node* n=new node(val);
+    n->next=head;
+    head=n;
+}
+void insertattail(node* &head,int val){
+    node* n=new node(val);
+    node* temp=head;
+    if(head==NULL){
+        head=n;
+        return;
+    }
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=n;
+}
+void display(node* head){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+void makecycle(node* &head,int pos){
+    node* temp=head;
+    node* startnode;
+    int count=1;
+    while(temp->next!=NULL){
+        if(count==pos){
+            startnode=temp;
+    }
+    temp=temp->next;
+    count++;
+}
+    temp->next=startnode;
+}
+bool detectcycle(node* &head){
+    node* slow=head;
+    node* fast=head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            return true;
+        }
+    }
+    return false;
+}
+void removecycle(node* &head){
+    node* slow=head;
+    node* fast=head;
+    do{
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    while(slow!=fast);
+        fast=head;
+    while(slow->next!=fast->next){
+        slow=slow->next;
+        fast=fast->next;
+    }
+    slow->next=NULL;
+}
+int main(){
+    node* head=NULL;
+    insertattail(head,1);
+    insertattail(head,2);
+    insertattail(head,3);
+    insertattail(head,4);
+    insertattail(head,5);
+    insertattail(head,6);
+    insertattail(head,7);
+    display(head);
+    insertathead(head,8);
+    insertathead(head,9);
+    display(head);
+    makecycle(head,4);
+    cout<<detectcycle(head)<<endl;
+    removecycle(head);
+    cout<<detectcycle(head)<<endl;
+    return 0;
+}
