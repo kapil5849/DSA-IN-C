adf03aa81517ddf6aad5c2f941162451f89c396d
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2021-10-12 13:31:10 +0530

    Create Reverse_the_Lindlist(recursive).cpp

---
+++ C++/linklist_Problem_in_c++/Reverse_the_Lindlist(recursive).cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+    node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+void insertathead(node* &head,int val){
+    node* n=new node(val);
+    n->next=head;
+    head=n;
+}
+void insertattail(node* &head,int val){
+    node* n=new node(val);
+    node* temp=head;
+    if(head==NULL){
+        head=n;
+        return;
+    }
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=n;
+}
+void display(node* head){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+node* reverserecursive(node* &head){
+    if(head==NULL || head->next==NULL){
+        return head;
+    }
+    node* newhead=reverserecursive(head->next);
+    head->next->next=head;
+    head->next=NULL;
+    return newhead;
+}
+int main(){
+    node* head=NULL;
+    insertattail(head,1);
+    insertattail(head,2);
+    insertattail(head,3);
+    insertattail(head,4);
+    display(head);
+    insertathead(head,5);
+    display(head);
+    node* newhead=reverserecursive(head);
+    display(newhead);
+    return 0;
+}
