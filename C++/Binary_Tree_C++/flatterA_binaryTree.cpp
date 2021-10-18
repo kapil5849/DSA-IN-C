e381fa21cc42629cf676b6dd853cc91954c2f221
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2021-10-18 23:19:05 +0530

    Create flatterA_binaryTree.cpp

---
+++ C++/Binary_Tree_C++/flatterA_binaryTree.cpp
@@ -0,0 +1,59 @@
+
+
+// flatter a binary tree...
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+void flatten(Node* root){
+    if(root==NULL || root->left==NULL || root->right==NULL){
+        return;
+    }
+    if(root->left!=NULL){
+        flatten(root->left);
+        Node* temp=root->right;
+        root->right=root->left;
+        root->left=NULL;
+        Node* t=root->right;
+        while(t->right!=NULL){
+            t=t->right;
+        }
+        t->right=temp;
+    }
+    flatten(root->right);
+}
+
+void inorderprint(Node* root){
+    if(root==NULL){
+        return;
+    }
+    inorderprint(root->left);
+    cout<<root->data<<" ";
+    inorderprint(root->right);
+}
+
+int main(){
+    struct Node* root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+    root->right->left=new Node(6);
+    root->right->right=new Node(7);
+    flatten(root);
+    inorderprint(root);
+    cout<<endl;
+    return 0;
+}
