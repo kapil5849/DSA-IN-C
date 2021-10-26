fb8519bf6d2dbbf7bcda31ec3124e123b4a54846
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2021-10-26 11:59:39 +0530

    Create NumberOfdigits.cpp

---
+++ C++/MathForDSA/NumberOfdigits.cpp
@@ -0,0 +1,16 @@
+
+
+// find the total number of digits...
+
+
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+
+int main(){
+    int n=6;
+    int b=2;
+    int ans=(int)(log(n)/log(b))+1;
+    cout<<ans<<endl;
+    return 0;
+}
