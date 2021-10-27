39656fc65cf959678ee71d8b085083a8a9eade92
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2021-10-27 09:41:43 +0530

    Update factor.cpp

--- C++/MathForDSA/factor.cpp
+++ C++/MathForDSA/factor.cpp
@@ -21,3 +21,32 @@ int main()
 
     return 0;
 }
+
+
+
+// time complexity O(sqrt(n))...
+
+
+#include <iostream>
+#include<math.h>
+using namespace std;
+int factor(int n){
+    for(int i=1;i<sqrt(n);i++){
+        if(n%i==0){
+            if(n/i==i){
+                cout<<i<<endl;
+            }
+            else{
+            cout<<i<<" "<<n/i<<endl;;
+            }
+        }
+    }
+}
+int main()
+{
+    int n=20;
+    cout<<factor(n);
+   
+
+    return 0;
+}
