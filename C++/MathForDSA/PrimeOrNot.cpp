11416e00aa47953497e2d3e6e889d0a287f69d35
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2021-10-27 06:12:41 +0530

    Update PrimeOrNot.cpp

--- C++/MathForDSA/PrimeOrNot.cpp
+++ C++/MathForDSA/PrimeOrNot.cpp
@@ -23,3 +23,30 @@ int main(){
    cout<<isPrime(n)<<endl;
     return 0;
 }
+
+
+// in range...
+
+
+#include<bits/stdc++.h>
+using namespace std;
+bool isPrime(int n){
+    if(n<=1){
+        return false;
+    }
+    int c=2;
+    while(c*c<=n){
+        if(n%c==0){
+            return false;
+        }
+        c++;
+    }
+    return true;
+}
+int main(){
+   int n=20;
+   for(int i=1;i<=n;i++){
+   cout<<isPrime(i)<<endl;
+   }
+    return 0;
+}
