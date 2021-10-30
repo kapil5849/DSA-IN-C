8fc06ab8721b9c6e2ecf353598971bf0d05cf0e3
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2021-10-31 02:16:36 +0530

    Create HashingSTL.cpp

---
+++ C++/Hashing_in_C++/HashingSTL.cpp
@@ -0,0 +1,31 @@
+
+
+// hashing in standard template llibrary STL...
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#include<set>
+#include<vector>
+#include<map>
+#include<unordered_map>
+#include<queue>
+#include<algorithm>
+#include<stack>
+#include<string>
+
+#define vi vector<int>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define rep(i,a,b) for(int i=0;i<b;i++)
+#define ff first
+#define ss second
+#define setBits(x) builtin_popcount(x)
+
+
+signed main(){
+    map<int,int> m;
+    m[5]=2;
+    cout<<m[5]<<endl;
+    return 0;
+}
