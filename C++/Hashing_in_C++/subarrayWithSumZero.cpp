d644f1094c69d7f7da4f770f8c020624b503bb8d
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2021-10-31 18:21:11 +0530

    Update subarrayWithSumZero.cpp

--- C++/Hashing_in_C++/subarrayWithSumZero.cpp
+++ C++/Hashing_in_C++/subarrayWithSumZero.cpp
@@ -17,7 +17,7 @@ using namespace std;
 #define vi vector<int>
 #define pii pair<int,int>
 #define vii vector<pii>
-#define rep(i,a,b) for(int i=0;i<b;i++)
+#define rep(i,a,b) for(int i=a;i<b;i++)
 #define ff first
 #define ss second
 #define setBits(x) builtin_popcount(x)
