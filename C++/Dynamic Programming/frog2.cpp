c84c381d91cf9054a2b4d391fcb067b2383ad60c
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2021-12-24 15:03:56 +0530

    Update frog2.cpp

--- C++/Dynamic Programming/frog2.cpp
+++ C++/Dynamic Programming/frog2.cpp
@@ -27,6 +27,46 @@ int main() {
     return 0;
 }
 
+// this code for dp element... 
+
+#include <bits/stdc++.h>
+using namespace std;
+const int N=1e5;
+int h[N],dp[N];
+int n,k;
+void go(int i){
+    cout<<i+1<<" ";
+    for(int kk=1;kk<=k and (i-kk)>=0;kk++){
+            if(dp[i]==dp[i-kk]+abs(h[i]-h[i-kk])){
+                go(i-kk);
+                return;
+            }
+        }
+}
+int main() {
+    cin>>n>>k;
+    for(int i=0;i<n;i++){
+        cin>>h[i];
+    }
+    dp[0]=0;
+    dp[1]=dp[0]+abs(h[1]-h[0]);
+    for(int i=2;i<n;i++){
+        dp[i]=INT_MAX;
+        for(int kk=1;kk<=k and (i-kk)>=0;kk++){
+            dp[i]=min(dp[i],dp[i-kk]+abs(h[i]-h[i-kk]));
+        }
+    }
+    cout<<dp[n-1]<<endl;
+    for(int i=0;i<n;i++){
+        cout<<dp[i]<<" ";
+    }
+    cout<<endl;
+    go(n-1);
+    return 0;
+}
+
 
 // top up...
+
+
 
