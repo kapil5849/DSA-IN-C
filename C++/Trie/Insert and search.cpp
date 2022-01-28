dd27e908d7c63bd9e62c5302942f54f2532e8c99
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2022-01-28 17:16:08 +0530

    Create Insert and search.cpp

---
+++ C++/Trie/Insert and search.cpp
@@ -0,0 +1,67 @@
+
+
+// basic operation of trie...
+
+
+#include<bits/stdc++.h>
+using namespace std;
+class Trie{
+    public:
+        class node{
+            public:
+                bool end;
+                node* next[26];
+                node(){
+                    end=false;
+                    for(int i=0;i<26;i++){
+                        next[i]=NULL;
+                    }
+                }
+        };
+        node* trie;
+        Trie(){
+            trie=new node();
+        }
+        void insert(string word){
+            int i=0;
+            node* it=trie;
+            while(i<word.size()){
+                if(it->next[word[i]-'a']==NULL)
+                    it->next[word[i]-'a']=new node();
+                it=it->next[word[i]-'a'];
+                i++;
+            }
+            it->end=true;
+        }
+        bool search(string word){
+            int i=0;
+            node* it=trie;
+            while(i<word.size()){
+                if(it->next[word[i]-'a']==NULL)
+                    return false;
+                it=it->next[word[i]-'a'];
+                i++;
+            }
+            return it->end;
+        }
+};
+int main(){
+    Trie *myTrie=new Trie();
+    vector<string> words={"kapil","elon","alien"};
+    for(auto w:words){
+        myTrie->insert(w);
+    }
+    if(myTrie->search("kapil")){
+        std::cout << "kapil found" << std::endl;
+    }
+    else{
+        cout<<"kapil does not found"<<endl;
+    }
+    if(myTrie->search("system")){
+        cout<<"system found"<<endl;
+    }
+    else{
+        cout<<"system does not exist"<<endl;
+    }
+    return 0;
+}
