23562dd0771fb6e2503dd84a79ec309917074104
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2022-04-24 22:49:39 +0530

    Update Abbreviation.cpp

--- pepcoding level 2/Recursion and Backtracking/Abbreviation.cpp
+++ pepcoding level 2/Recursion and Backtracking/Abbreviation.cpp
@@ -2,6 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 void solution(string str,string asf,int cnt,int pos){
+    // base case...
     if(str.length()==pos){
         if(cnt>0){
             cout<<asf<<cnt<<endl;
@@ -10,17 +11,27 @@ void solution(string str,string asf,int cnt,int pos){
         }
         return;
     }
+    // call for yes...
     if(cnt>0){
         solution(str,asf+to_string(cnt)+str[pos],0,pos+1);
     }
     else{
         solution(str,asf+str[pos],0,pos+1);
     }
+    // call for no...
     solution(str,asf,cnt+1,pos+1);
 }
 int main(){
     string str;
     cin>>str;
+    // call...
     solution(str,"",0,0);
     return 0;
 }
+
+
+// this is based on level and option...
+// level => word...
+// option => yes and no call...
+// in yes call => make cnt=0,pos++,
+// in no call=> cnt++,pos++,
